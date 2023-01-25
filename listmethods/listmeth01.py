4b41f1cf31a7e113796561c6bad065e59140e8a0
rholt18698 <thebigapplerob@gmail.com>
2023-01-24 19:46:56 -0500

    Added Comments

--- listmethods/listmeth01.py
+++ listmethods/listmeth01.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-proto = ["ssh", "http", "https"] #Proto list
+proto = ["ssh", "http", "https"]
 print(proto)
-print(proto[1])  # print http
+print(proto[1])
 proto.extend("dns") # this line will add d, n, and s
 print(proto)
 
